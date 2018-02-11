a99967704a4f785288c6fc8f07abe248af723855
alisonbsu <alison@thinkecoinc.com>
2018-02-11 00:00:46 -0500

    initial commit

---
+++ MapTheCollections.CollectionValidator/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MapTheCollections.CollectionValidator.Controllers {
+    public class HomeController : Controller {
+
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult CSVUpload() {
+            return View();
+        }
+
+        public ActionResult ManualInput() {
+            ViewBag.Message = "Your manual input page.";
+
+            return View();
+        }
+    }
+}
---
+++ MapTheCollections.CollectionValidator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MapTheCollections.CollectionValidator.MvcApplication" Language="C#" %>
