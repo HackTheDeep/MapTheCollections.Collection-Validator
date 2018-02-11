627cbe64aa7e132fd6eb9d52c2b113447275c764
alisonbsu <alison@thinkecoinc.com>
2018-02-11 09:41:34 -0500

    Incorporated validator into manual-input

--- MapTheCollections.CollectionValidator/Scripts/app/manual-input.js
+++ MapTheCollections.CollectionValidator/Scripts/app/manual-input.js
@@ -1,5 +1,124 @@
 ﻿$(document).ready(function () {
 
+    var MyLocationField = function (config) {
+        jsGrid.Field.call(this, config);
+    };
+
+
+    MyLocationField.prototype = new jsGrid.Field({
+
+        css: "date-field",            // redefine general property 'css'
+        address_components: "",    // TODO: If one of these, show only that type: https://developers.google.com/places/supported_types
+
+        sorter: function (loc1, loc2) {
+            return loc1 < loc2;
+        },
+
+        itemTemplate: function (value) {
+            console.log(this._insertPicker[0].value, value);
+            let x = localStorage.getItem(value);
+
+            return value;
+        },
+
+        insertTemplate: function (value) {
+            this._insertPicker = $("<input>");
+            var autocomplete = new google.maps.places.Autocomplete(this._insertPicker[0]);
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                let addressMetta = {};
+                addressMetta.types = autocomplete.getPlace().address_components;
+                addressMetta.lat = autocomplete.getPlace().geometry.location.lat();
+                addressMetta.lng = autocomplete.getPlace().geometry.location.lng();
+                console.log(autocomplete.getPlace(), addressMetta);
+                sessionStorage.setItem(autocomplete.getPlace().formatted_address, JSON.stringify(addressMetta));
+            });
+            return this._insertPicker;
+        },
+
+        editTemplate: function (value) {
+            this._insertPicker = $("<input>");
+            var autocomplete = new google.maps.places.Autocomplete(this._insertPicker[0]);
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                let addressMetta = {};
+                addressMetta.types = autocomplete.getPlace().address_components;
+                addressMetta.lat = autocomplete.getPlace().geometry.location.lat();
+                addressMetta.lng = autocomplete.getPlace().geometry.location.lng();
+                console.log(autocomplete.getPlace(), addressMetta);
+                sessionStorage.setItem(autocomplete.getPlace().formatted_address, JSON.stringify(addressMetta));
+            });
+            return this._insertPicker;
+        },
+
+        insertValue: function () {
+            return this._insertPicker[0].value;
+        },
+
+        editValue: function () {
+            return this._insertPicker[0].value;
+        }
+    });
+
+
+    jsGrid.fields.location = MyLocationField;
+
+    var MyDateField = function (config) {
+        jsGrid.Field.call(this, config);
+    };
+
+    MyDateField.prototype = new jsGrid.Field({
+
+        css: "date-field",            // redefine general property 'css'
+        align: "center",              // redefine general property 'align'
+
+        myCustomProperty: "foo",      // custom property
+
+        sorter: function (date1, date2) {
+            return new Date(date1) - new Date(date2);
+        },
+
+        itemTemplate: function (value) {
+            //       var moment = require('moment');
+            // moment().format();
+            // console.log(value, moment(value, "YYYY-MMMM-D"));
+
+            return new Date(value).toDateString();
+        },
+
+        insertTemplate: function (value) {
+            return this._insertPicker = $("<input>").datepicker({ defaultDate: new Date() });
+        },
+
+        editTemplate: function (value) {
+            return this._editPicker = $("<input>").datepicker().datepicker("setDate", new Date(value));
+        },
+
+        insertValue: function () {
+            return this._insertPicker.datepicker("getDate").toISOString();
+        },
+
+        editValue: function () {
+            return this._editPicker.datepicker("getDate").toISOString();
+        }
+    });
+
+    jsGrid.fields.date = MyDateField;
+
+
+
+    // ************************** //
+    // **  CONTINENT DROPDOWN  ** //
+    // ************************** //
+    var continents = [
+        { Name: "Africa", Id: 0 },
+        { Name: "Antarctica", Id: 1 },
+        { Name: "Asia", Id: 2 },
+        { Name: "Europe", Id: 3 },
+        { Name: "North America", Id: 4 },
+        { Name: "Oceania", Id: 5 },
+        { Name: "South America", Id: 6 }
+    ];
+
+
     //var clients = [
     //    { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
     //    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
@@ -48,8 +167,9 @@
 
         //data: exampleData,
 
-        fields: [
-            { name: "Locality Bay/ Sound/ Harbor", type: "text" },
+        fields: [   
+
+            { name: "Locality Bay/ Sound/ Harbor", type: "location" },
             { name: "Locality Continent", type: "text" },
             { name: "Locality Country ", type: "text" },
             { name: "Locality Dept/ Province/ State", type: "text" },
@@ -90,6 +210,7 @@
 
         ]
     });
+
 
 
     $('button#export-csv').click(function (evt) {
